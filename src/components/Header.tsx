1780dccd60fb64d5550df29d6e4731cd9145f145
Paulo Sarmento <paulo.cesar.sarmento@hotmail.com>
2024-02-21 13:14:55 -0300

    Header Links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,20 @@ import React from "react";
 export default function Header() {
   return (
     <header className="fixed top-0 z-10 w-full flex items-center justify-between px-4 py-4 lg:px-10 lg:py-6">
-      <img src="https://rb.gy/ulxxee" alt="netflix" width={120} height={120} />
-      <ul>
-        <li>Home</li>
-        <li>TV Shows</li>
-        <li>Movies</li>
-        <li>Latest</li>
-      </ul>
+      <div className="flex items-center space-x-2 md:space-x-8">
+        <img
+          src="https://rb.gy/ulxxee"
+          alt="netflix"
+          width={120}
+          height={120}
+        />
+        <ul className="hidden md:space-x-4 md:flex">
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>Latest</li>
+        </ul>
+      </div>
     </header>
   );
 }
