b8b1d224496e64fa2e2caa2b208155ef8b82cc20
Paulo Sarmento <paulo.cesar.sarmento@hotmail.com>
2024-02-21 21:29:00 -0300

    Icones e Plugins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import { PlayIcon } from "@heroicons/react/24/solid";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,9 +32,11 @@ export default function Home() {
 
         <div className="flex space-x-3">
           <button className="md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5">
+            <PlayIcon className="h-6" />
             Play
           </button>
           <button className="md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5">
+            <InformationCircleIcon className="h-6" />
             More Info
           </button>
         </div>
